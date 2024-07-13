77c7fee505084c643b0059173a43d6fb7d53c62e
elshankhanlari <elshankhanlari@gmail.com>
2024-07-13 19:05:21 +0200

    Tested-Version

--- switched-ovs.sh
+++ switched-ovs.sh
@@ -45,6 +45,8 @@ ip link add veth-yellow1 type veth peer name veth-yellow1-br
 ip link add veth-green1 type veth peer name veth-green1-br
 ip link add veth-yellow2 type veth peer name veth-yellow2-br
 ip link add veth-green2 type veth peer name veth-green2-br
+ip link add veth-router-100 type veth peer name veth-r-br-100
+ip link add veth-router-200 type veth peer name veth-r-br-200
 sleep 1
 
 echo "Moving interfaces into the namespaces"
@@ -52,7 +54,8 @@ ip link set veth-yellow1 netns yellow1
 ip link set veth-green1 netns green1
 ip link set veth-yellow2 netns yellow2
 ip link set veth-green2 netns green2
-ip link set router-trunk netns router
+ip link set veth-router-100 netns router
+ip link set veth-router-200 netns router
 sleep 1
 
 echo "---> Attaching interfaces to ovs-switch"
@@ -60,6 +63,8 @@ ovs-vsctl add-port br1 veth-yellow1-br
 ovs-vsctl add-port br1 veth-green1-br
 ovs-vsctl add-port br2 veth-yellow2-br
 ovs-vsctl add-port br2 veth-green2-br
+ovs-vsctl add-port br3 veth-r-br-100
+ovs-vsctl add-port br3 veth-r-br-200
 sleep 1
 
 echo "Configuring IP addresses..."
@@ -75,13 +80,16 @@ ovs-vsctl set port veth-yellow1-br tag=100
 ovs-vsctl set port veth-green1-br tag=200
 ovs-vsctl set port veth-yellow2-br tag=100
 ovs-vsctl set port veth-green2-br tag=200
+ovs-vsctl set port veth-r-br-100 tag=100
+ovs-vsctl set port veth-r-br-200 tag=200
 
 echo "Turning up the interfaces within namespaces..."
 ip -n yellow1 link set veth-yellow1 up
 ip -n green1 link set veth-green1 up
 ip -n yellow2 link set veth-yellow2 up
 ip -n green2 link set veth-green2 up
-ip -n green2 link set veth-green2 up
+ip -n router link set veth-router-100 up
+ip -n router link set veth-router-200 up
 sleep 1
 
 echo "Turning up the interfaces within default namespace..."
@@ -89,6 +97,8 @@ sudo ip link set veth-yellow1-br up
 sudo ip link set veth-green1-br up
 sudo ip link set veth-yellow2-br up
 sudo ip link set veth-green2-br up
+sudo ip link set veth-r-br-100 up
+sudo ip link set veth-r-br-200 up
 echo "--- OVS show ---"
 
 ovs-vsctl show
@@ -96,7 +106,6 @@ ovs-vsctl show
 echo "--- Creating link between switches..."
 ip link add br1-trunk type veth peer name br1-br3-trunk
 ip link add br2-trunk type veth peer name br2-br3-trunk
-ip link add br3-r-trunk type veth peer name router-trunk
 sleep 1
 
 echo "--- Add trunk port to switches..."
@@ -104,31 +113,27 @@ ovs-vsctl add-port br1 br1-trunk
 ovs-vsctl add-port br3 br1-br3-trunk
 ovs-vsctl add-port br3 br2-br3-trunk
 ovs-vsctl add-port br2 br2-trunk
-ovs-vsctl add-port br3 br3-r-trunk
 sleep 1
 
 echo "--- Turning up trunk interfaces..."
-sudo ip link set br1-trunk up
-sudo ip link set br2-trunk up
-sudo ip link set br3-router-trunk up
-sudo ip link set br1-br3-trunk up
-sudo ip link set br2-br3-trunk up
-sudo ip link set router-trunk up
+ip link set br1-trunk up
+ip link set br2-trunk up 
+ip link set br1-br3-trunk up
+ip link set br2-br3-trunk up
 sleep 1
 
 echo "--- OVS show..."
 ovs-vsctl show
 
-echo "--- Set VLAN trunk port... VLAN 100 intentionally not included in trunk"
+echo "--- Set VLAN trunk port..."
 ovs-vsctl set port br1-trunk trunks=100,200
 ovs-vsctl set port br2-trunk trunks=100,200
 ovs-vsctl set port br1-br3-trunk trunks=100,200
 ovs-vsctl set port br2-br3-trunk trunks=100,200
-ovs-vsctl set port br3-router-trunk trunks=100,200
 
 echo "Enable routing ..."
-ip netns exec router ip addr add 192.168.100.1/24 dev router-trunk
-ip netns exec router ip addr add 192.168.200.1/24 dev router-trunk
+ip netns exec router ip addr add 192.168.100.1/24 dev veth-router-100
+ip netns exec router ip addr add 192.168.200.1/24 dev veth-router-200
 sudo ip netns exec router sysctl -w net.ipv4.ip_forward=1
 
 echo "--- Show OVS..."
