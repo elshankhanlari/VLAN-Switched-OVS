49ab799d949af418b63b56d0a8d1cfa3080cb26c
elshankhanlari <elshankhanlari@gmail.com>
2024-07-14 10:10:10 +0200

    clean-environmen.sh modified

--- clean-environment.sh
+++ clean-environment.sh
@@ -4,15 +4,15 @@ echo "---Deleteing ovs bridges..."
 ovs-vsctl del-br br1
 ovs-vsctl del-br br2
 ovs-vsctl del-br br3
-sleep1
 
 echo "---Setting up environment..."
 ip route flush table main
 systemctl restart networking
 systemctl restart openvswitch-switch
-sleep1
+sleep 1
 
 echo "---echo Verify Clean Environment..."
 ifconfig
 ovs-vsctl show
+route -n
 ip addr show
