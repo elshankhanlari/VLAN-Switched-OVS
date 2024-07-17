eb1625dd456d35eb8e428ab9a14772fd0ff3975e
elshankhanlari <elshankhanlari@gmail.com>
2024-07-17 12:41:45 +0200

    modified

--- mininet-scripts/topo-vlan.py
+++ mininet-scripts/topo-vlan.py
@@ -27,9 +27,10 @@ class CustomTopo(Topo):
         router = self.addHost('router', cls=LinuxRouter)
 
         # Add links between switches
-        self.addLink(s1, s3)
-        self.addLink(s3, s2)
-        self.addLink(router, s3)
+        self.addLink(s1, s3, intfName1='br1-trunk')
+        self.addLink(s3, s2, intfName1='br2-trunk')
+        self.addLink(router, s3, intfName1='router-eth0.100')
+        self.addLink(router, s3, intfName1='router-eth0.200')
 
         # Add hosts
         h1 = self.addHost('h1', ip='192.168.100.2/24', defaultRoute='via 192.168.100.1')
@@ -38,10 +39,16 @@ class CustomTopo(Topo):
         h4 = self.addHost('h4', ip='192.168.200.3/24', defaultRoute='via 192.168.200.1')
 
         # Add links between hosts and switches with VLANs
-        self.addLink(h1, s1, params1={'vlan': 100})
-        self.addLink(h2, s1, params1={'vlan': 200})
-        self.addLink(h3, s2, params1={'vlan': 100})
-        self.addLink(h4, s2, params1={'vlan': 200})
+        self.addLink(h1, s1, intfName1='br1-eth1')
+        self.addLink(h2, s1, intfName1='br1-eth2')
+        self.addLink(h3, s2, intfName1='br2-eth3')
+        self.addLink(h4, s2, intfName1='br2-eth4')
+
+        # Configure trunk behaviour
+        s1.cmd('ovs-vsctl set Port br1-trunk tag=[]')
+        s1.cmd('ovs-vsctl set Port br2-trunk tag=[]')
+        s2.cmd('ovs-vsctl set Port br2-eth3 tag=[]')
+        s2.cmd('ovs-vsctl set Port br2-eth4 tag=[]')
 
 
 def run():
@@ -61,8 +68,8 @@ def run():
     router.cmd('ifconfig router-eth0.200 192.168.200.1/24')
 
     # Add routing for reaching networks that aren't directly connected
-    info(router.cmd("ip route add 192.168.100.0/24 dev router-eth0.100"))
-    info(router.cmd("ip route add 192.168.200.0/24 dev router-eth0.200"))
+    info(router.cmd("ip route add 192.168.100.1/24 dev router-eth0.100"))
+    info(router.cmd("ip route add 192.168.200.1/24 dev router-eth0.200"))
 
     CLI(net)
     net.stop()
