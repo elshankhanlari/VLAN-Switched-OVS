23e67ff3d8ef1a1d2868a9a244ec32bbdf269826
elshankhanlari <elshankhanlari@gmail.com>
2024-07-24 18:35:46 +0200

    topo-vlan-param modified

--- mininet-scripts/topo-vlan-param.py
+++ mininet-scripts/topo-vlan-param.py
@@ -40,16 +40,14 @@ class CustomTopo(Topo):
 
         # Add hosts and assign them to VLANs
         for i in range(1, middle_switch_index + 1):
-            vlan_id = 100 if i % 2 == 1 else 200
-            host1 = self.addHost(f'h{2*i-1}', ip=f'192.168.{vlan_id}.{2*i-1}/24', defaultRoute=f'via 192.168.{vlan_id}.0')
-            host2 = self.addHost(f'h{2*i}', ip=f'192.168.{vlan_id}.{2*i}/24', defaultRoute=f'via 192.168.{vlan_id}.0')
+            host1 = self.addHost(f'h{2*i-1}', ip=f'192.168.{100}.{2*i-1}/24', defaultRoute=f'via 192.168.{100}.0')
+            host2 = self.addHost(f'h{2*i}', ip=f'192.168.{200}.{2*i}/24', defaultRoute=f'via 192.168.{200}.0')
             self.addLink(host1, switches[i - 1], intfName1=f'br{i}-eth1')
             self.addLink(host2, switches[i - 1], intfName1=f'br{i}-eth2')
 
         for i in range(middle_switch_index + 2 , num_switches + 1):
-            vlan_id = 100 if i % 2 == 1 else 200
-            host1 = self.addHost(f'h{(2*i-1) - 2}', ip=f'192.168.{vlan_id}.{(2*i-1) - 2}/24', defaultRoute=f'via 192.168.{vlan_id}.0')
-            host2 = self.addHost(f'h{(2*i) - 2}', ip=f'192.168.{vlan_id}.{(2*i) - 2}/24', defaultRoute=f'via 192.168.{vlan_id}.0')
+            host1 = self.addHost(f'h{(2*i-1) - 2}', ip=f'192.168.{100}.{(2*i-1) - 2}/24', defaultRoute=f'via 192.168.{100}.0')
+            host2 = self.addHost(f'h{(2*i) - 2}', ip=f'192.168.{200}.{(2*i) - 2}/24', defaultRoute=f'via 192.168.{200}.0')
             self.addLink(host1, switches[i - 1], intfName1=f'br{i}-eth1')
             self.addLink(host2, switches[i - 1], intfName1=f'br{i}-eth2')
 
